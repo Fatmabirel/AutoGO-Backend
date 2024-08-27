57a5fc00041a35348155baf5a8ccf8d06d34331e
Fatma Birel <fatmabrl11@gmail.com>
2024-08-27 13:32:35 +0300

    Add IEntityRepository interface for generic repository pattern

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // Generic Repository : tasarım desenidir. Temel CRUD işlemlerini tanımlar
+    // Generic Constraint -> T türünü sınırlar
+    // class : referans tür
+    // IEntity: IEntity olan ya da IEntity türünü implemente eden
+    // new : newlenebilir (Yani interfaceler(IEntity) kullanılamaz
+    public interface IEntityRepository<T> where T : class, IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
