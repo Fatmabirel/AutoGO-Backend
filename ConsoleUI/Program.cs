a612df34cec4a282941b4a18367864463ca8da3b
Fatma Birel <fatmabrl11@gmail.com>
2024-08-27 09:55:02 +0300

    Implement CRUD operations for Car class

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarManager carManager = new ICarManager(new InMemoryCarDal());
+
+            Console.WriteLine("List of cars:");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine($"{car.Id} - {car.Description}, Model Year: {car.ModelYear}, Price: {car.DailyPrice}");
+            }
+
+            // Add New Car
+            Car newCar = new Car
+            {
+                Id = 11,
+                BrandId = 3,
+                ColorId = 2,
+                ModelYear = 2022,
+                DailyPrice = 500,
+                Description = "New Mercedes-Benz E-Class"
+            };
+
+            carManager.Add(newCar);
+            Console.WriteLine("\nAdded a new car:");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine($"{car.Id} - {car.Description}, Model Year: {car.ModelYear}, Price: {car.DailyPrice}");
+            }
+
+            // Update a car
+            Car carToUpdate = carManager.GetById(1);
+
+            carToUpdate.Description = "Updated Audi A4";
+            carToUpdate.DailyPrice = 275;
+            carManager.Update(carToUpdate);
+            Console.WriteLine("\nUpdated the first car:");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine($"{car.Id} - {car.Description}, Model Year: {car.ModelYear}, Price: {car.DailyPrice}");
+            }
+
+            // Delete a car
+            carManager.Delete(carManager.GetById(2));
+            Console.WriteLine("\nDeleted the second car:");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine($"{car.Id} - {car.Description}, Model Year: {car.ModelYear}, Price: {car.DailyPrice}");
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,71 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car { Id = 1, BrandId = 1, ColorId = 1, ModelYear = 2018, DailyPrice = 250, Description = "Audi A4" },
+                new Car { Id = 2, BrandId = 2, ColorId = 2, ModelYear = 2019, DailyPrice = 300, Description = "BMW 3 Series" },
+                new Car { Id = 3, BrandId = 3, ColorId = 3, ModelYear = 2020, DailyPrice = 350, Description = "Mercedes-Benz C-Class" },
+                new Car { Id = 4, BrandId = 4, ColorId = 4, ModelYear = 2017, DailyPrice = 220, Description = "Volkswagen Golf" },
+                new Car { Id = 5, BrandId = 5, ColorId = 5, ModelYear = 2021, DailyPrice = 400, Description = "Tesla Model 3" },
+                new Car { Id = 6, BrandId = 1, ColorId = 2, ModelYear = 2020, DailyPrice = 320, Description = "Audi Q5" },
+                new Car { Id = 7, BrandId = 2, ColorId = 3, ModelYear = 2018, DailyPrice = 280, Description = "BMW X5" },
+                new Car { Id = 8, BrandId = 3, ColorId = 4, ModelYear = 2019, DailyPrice = 330, Description = "Mercedes-Benz GLC" },
+                new Car { Id = 9, BrandId = 4, ColorId = 5, ModelYear = 2017, DailyPrice = 210, Description = "Volkswagen Passat" },
+                new Car { Id = 10, BrandId = 5, ColorId = 1, ModelYear = 2021, DailyPrice = 450, Description = "Tesla Model S" }
+            };
+        }
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            if (carToDelete != null)
+            {
+                _cars.Remove(carToDelete);
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+           return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            if (carToUpdate != null)
+            {
+                carToUpdate.BrandId = car.BrandId;
+                carToUpdate.ColorId = car.ColorId;
+                carToUpdate.ModelYear = car.ModelYear;
+                carToUpdate.DailyPrice = car.DailyPrice;
+                carToUpdate.Description = car.Description;
+            }
+        }
+
+    }
+}
